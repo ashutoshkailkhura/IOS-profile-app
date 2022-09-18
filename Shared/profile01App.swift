ba3e198072d14c24f8791253275836eda60ceaf8
ashutoshkailkhura <ashi3377@gmail.com>
2022-09-18 07:52:45 +0530

    Initial Commit

---
+++ Shared/profile01App.swift
@@ -0,0 +1,17 @@
+//
+//  profile01App.swift
+//  Shared
+//
+//  Created by ashutosh kailkhura on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct profile01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
